1c0cd8a29d7019b28607610e238c581c653c0c12
Oliver Mueller <oliver.mueller@esrlabs.com>
2020-01-21 20:52:31 +0100

    [](fix) changed build + workflow

--- jekyll/README.md
+++ jekyll/README.md
@@ -1,3 +1,4 @@
 # Chipmunk documentation
 
 This repository contains documentation and the web page for chipmunk.
+
---
+++ rakefile.rb
@@ -0,0 +1,37 @@
+require 'rake/clean'
+
+GITHUB_TOKEN=ENV['GITHUB_TOKEN']
+GITHUB_ACTOR='marcmo'
+GITHUB_REPOSITORY='esrlabs/chipmunk-docs'
+INPUT_GIT_COMMIT_MESSAGE="generated site content"
+SITE='jekyll/_site'
+DOCS='jekyll/_site/book'
+
+CLEAN.include(SITE)
+
+desc 'install'
+task :install do
+  sh "bundle install"
+end
+
+desc 'build jekyll site'
+task :build_jekyll do
+  cd 'jekyll' do
+    sh 'bundle exec jekyll build'
+  end
+  sh "touch ./#{SITE}/.nojekyll"
+end
+
+desc 'build'
+task :build => [SITE, DOCS]
+
+desc 'build mdbook documentation'
+task :book do
+  cd "book" do
+    sh 'mdbook build'
+    mv 'book', "../#{DOCS}"
+  end
+end
+
+file SITE => :build_jekyll
+file DOCS => :book
