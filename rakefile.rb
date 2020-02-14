2aaae38fb5ee4adba3555560cc0ab0e6ea7f8f03
Oliver Mueller <oliver.mueller@esrlabs.com>
2020-02-14 13:29:19 +0100

    [](fix) navigation correction
    rakefile cleanup

--- rakefile.rb
+++ rakefile.rb
@@ -1,32 +1,43 @@
+# frozen_string_literal: true
+
 require 'rake/clean'
 
-GITHUB_TOKEN=ENV['GITHUB_TOKEN']
-GITHUB_ACTOR='marcmo'
-GITHUB_REPOSITORY='esrlabs/chipmunk-docs'
-INPUT_GIT_COMMIT_MESSAGE="generated site content"
-SITE_DIR='jekyll/_site'
-SITE="#{SITE_DIR}/index.html"
-DOCS='jekyll/_site/book'
+GITHUB_TOKEN = ENV['GITHUB_TOKEN']
+GITHUB_ACTOR = 'marcmo'
+GITHUB_REPOSITORY = 'esrlabs/chipmunk-docs'
+INPUT_GIT_COMMIT_MESSAGE = 'generated site content'
+SITE_DIR = 'jekyll/_site'
+SITE = "#{SITE_DIR}/index.html"
+DOCS = 'jekyll/_site/book'
+MDBOOK_DIR = 'book'
 
 directory SITE_DIR
 CLEAN.include(SITE_DIR)
 
+task :clean => :clean_book
+
+task :clean_book do
+  cd MDBOOK_DIR do
+    sh 'mdbook clean'
+  end
+end
+
 desc 'install'
 task :install do
-  sh "bundle install"
+  sh 'bundle install'
 end
 
 desc 'build jekyll site'
 task :build_jekyll do
   cd 'jekyll' do
-    sh """
+    sh ''"
       docker run \
         -v \"$(pwd):/workspace\" \
         -w /workspace \
         --user #{Process.uid}:#{Process.gid} \
         meatlink/chipmunk-docs-builder-gha:latest \
         bundle exec jekyll build
-    """
+    "''
   end
   sh "touch ./#{SITE_DIR}/.nojekyll"
 end
@@ -43,19 +54,19 @@ task :build => [SITE, DOCS]
 
 desc 'build mdbook documentation'
 task :book => SITE_DIR do
-  cd "book" do
+  cd MDBOOK_DIR do
     sh 'mdbook build'
     mv 'book', "../#{DOCS}"
   end
 end
 
 task :clean_docs do
-  rm_r DOCS if Dir.exists?(DOCS)
+  rm_r DOCS if Dir.exist?(DOCS)
 end
 desc 'python webserver'
 task :preview_mdbook do
-  cd 'book' do
-    sh "mdbook serve"
+  cd MDBOOK_DIR do
+    sh 'mdbook serve'
   end
 end
 
