53bb0e919935e0c317276da0623458f68eea30ec
fur-gan <56543045+fur-gan@users.noreply.github.com>
2020-02-18 09:15:06 +0100

    [#6](feat) Show code example as default

--- book/src/scripts/script.tab.js
+++ book/src/scripts/script.tab.js
@@ -3,12 +3,12 @@ function openCode(evt, fileName) {
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     if (tabcontent[i].className.split(' ')[1] === evt.currentTarget.parentElement.className.split(' ')[1]) {
-      tabcontent[i].className = tabcontent[i].className.replace(/active/gi, '');
+      tabcontent[i].className = tabcontent[i].className.replace(/ active/gi, '');
     }
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(/active/gi, '');
+    tablinks[i].className = tablinks[i].className.replace(/ active/gi, '');
   }
   document.getElementById(fileName).className += " active";
   evt.currentTarget.className += " active";
